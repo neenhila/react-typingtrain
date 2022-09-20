d1e8a95ecc529010aae61779afd1099cf1b4223f
Neenhila <83518175+neenhila@users.noreply.github.com>
2022-09-21 00:42:29 +0300

    Update Word.jsx

--- src/Word.jsx
+++ src/Word.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
-import ReactHTMLParser from 'react-html-parser'
 import './word.css'
 import FinishScore from './FinishScore.jsx'
 function Word(props) {
 
     return (
-        <div className="words">
-            {(props.things.DataisLoaded && !props.things.isFinished && props.things.items.length > 0) ? ReactHTMLParser(props.things.itemhtml) : ((props.things.DataisLoaded && props.things.isFinished) ? <FinishScore score={props.score} maxScore={props.maxScore} timeFinish={props.timeFinish} /> : <h1>Kelimeler alınıyor. Lütfen bekleyin...</h1>)}
+        <div className="words" id="words">
+            {(props.things.DataisLoaded && !props.things.isFinished && props.things.items.length > 0) ? props.things.items.map(item => {
+                return (<p className='word' id={item.madde_id} key={item.madde_id}>{item.madde}</p>)
+            }) : ((props.things.DataisLoaded && props.things.isFinished) ? <FinishScore score={props.score} maxScore={props.maxScore} timeFinish={props.timeFinish} /> : <h1>Kelimeler alınıyor. Lütfen bekleyin...</h1>)}
         </div>
     )
 }
